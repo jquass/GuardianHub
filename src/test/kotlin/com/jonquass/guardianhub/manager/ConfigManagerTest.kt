99c479eeb4eb588965f1732d2772aa40d86654af
Jon Quass <quassj@gmail.com>
2026-03-08 15:08:39 -0400

    ConfigManagerTest Improvements (#36)
    * ConfigManagerTest Improvements
    
    * Spotless

--- src/test/kotlin/com/jonquass/guardianhub/manager/ConfigManagerTest.kt
+++ src/test/kotlin/com/jonquass/guardianhub/manager/ConfigManagerTest.kt
@@ -126,6 +126,28 @@ class ConfigManagerTest {
   }
 
   @Test
+  fun `readConfig should ignore keys without values`() {
+    tempFile.writeText("GUARDIAN_IP")
+
+    val result = ConfigManager.readConfig()
+
+    assertThat(result.isSuccess).isTrue()
+    val response = result.getOrThrow()
+    assertThat(response.entries).isEmpty()
+    assertThat(response.categories).isEmpty()
+  }
+
+  @Test
+  fun `readConfig should return error if file is unreadable`() {
+    tempFile.setReadable(false)
+
+    val result = ConfigManager.readConfig()
+
+    assertThat(result.isError).isTrue
+    tempFile.setReadable(true)
+  }
+
+  @Test
   fun `getRawConfigValue should return value for existing key`() {
     tempFile.writeText("GUARDIAN_IP=0.0.0.1\n")
 
